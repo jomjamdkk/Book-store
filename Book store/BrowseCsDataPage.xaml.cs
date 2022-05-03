56f346422d8d053613b90998dab5ccfc1694417c
jomjam dkk <jomjamdkk@gmail.com>
2022-05-04 00:16:17 +0700

    Revert "Browse Cs dt"
    This reverts commit bb8fc09924c540adeb5e6b0b52d8441e5931b51e.

--- Book store/BrowseCsDataPage.xaml.cs
+++ Book store/BrowseCsDataPage.xaml.cs
@@ -19,7 +19,6 @@ namespace Book_store {
     /// Interaction logic for BrowseCsDataPage.xaml
     /// </summary>
     public partial class BrowseCsDataPage : Page {
-        private bool SearchBoxClicked=false;
         public BrowseCsDataPage() {
             InitializeComponent();
             NotFound.Visibility = Visibility.Hidden;
@@ -31,7 +30,6 @@ namespace Book_store {
 
         private void SearchByID_Click(object sender, RoutedEventArgs e)
         {
-            
             ArrayList Data = Customers.get(int.Parse(SearchBox.Text));
             if (Data.Count != 0)
             {
@@ -41,52 +39,13 @@ namespace Book_store {
                 EmailBlock.Text = Data[3].ToString();
                 NotFound.Visibility = Visibility.Hidden;
             }
-            else {
-                Not_Found();
-            }
-        }
-
-        private void SearchByEmail_Click(object sender, RoutedEventArgs e)
-        {
-
-            ArrayList Data = Customers.get(SearchBox.Text);
-            if (Data.Count != 0)
-            {
-                IDBlock.Text = Data[0].ToString();
-                NameBlock.Text = Data[1].ToString();
-                AddressBlock.Text = Data[2].ToString();
-                EmailBlock.Text = Data[3].ToString();
-                NotFound.Visibility = Visibility.Hidden;
-            }
-            else
-            {
-                Not_Found();
-            }
-
-        }
-        private void Not_Found() 
-        {
-
-            NotFound.Visibility = Visibility.Visible;
-            string s = "Not found";
-            IDBlock.Text = s;
-            NameBlock.Text = s;
-            AddressBlock.Text = s;
-            EmailBlock.Text = s;
-        }
-
-
-        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (!SearchBoxClicked)
-            {
-                SearchBox.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00183E"));
-                SearchBox.Text = "";
-                SearchBoxClicked = true;
+            else { NotFound.Visibility = Visibility.Visible;
+                string s = "Not found";
+                IDBlock.Text = s;
+                NameBlock.Text = s;
+                AddressBlock.Text = s;
+                EmailBlock.Text = s;
             }
         }
     }
 }
-//#FF00183E DarkBlue
-//#FF828282 Gray
-//new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString(""));
